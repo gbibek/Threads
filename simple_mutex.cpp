850c1585a47d5363d732b4f9ea1d600469fb87bc
Bibek Ghimire <bghimi4@lsu.edu>
2015-01-28 16:56:14 -0600

    cleaning code

--- simple_mutex.cpp
+++ simple_mutex.cpp
@@ -18,24 +18,16 @@ void adder(int &i)
    // cout<<"From thread id "<<this_thread::get_id()<<endl;
 }
 
-
-
 int main()
 {
-
     vector<thread> threads;
     int value = 0;
     cout<<"Initial value = "<<value<<endl;
     for(int i = 0; i < 200; ++i)
-    {
-    
+    {   
         threads.push_back(thread(adder,ref(value)));
-
     }
     for_each(threads.begin(),threads.end(), mem_fn(&thread::join));   
- 
     cout<<"After Threads add, the result is "<<value<<endl;
-
-
 return 0;
 }
